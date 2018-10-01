4240310f3f2189a4c27cfca0ec06c692eca2d16e
Pavan Kumar <digitalmonkeypa1@gmail.com>
2018-10-01 17:00:13 +0530

    chat completed

--- frontend/WebContent/services/jobservice.js
+++ frontend/WebContent/services/jobservice.js
@@ -3,7 +3,7 @@
  */
 	app.factory('JobService',function($http){
 	var jobService={}
-	var BASE_URL = "http://localhost:9080/colloboration_middleware"
+	var BASE_URL = "http://localhost:9090/colloboration_middleware"
 		
 		
 	jobService.addJob=function(job){
